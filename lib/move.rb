f62a1b9a065ab18756e85c296b3033d5ef2e7d81
Raul Sposito <raulsposito@gmail.com>
2019-07-05 09:56:26 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,14 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+#CHANGE INFO TO INTEGER
+def input_to_index(move)
+   index = move.to_i - 1
+end
+
+
+#MAKE MOVE ON BOARD
+def move(board, index, token = "X")
+  board[index] = token
+end
+
